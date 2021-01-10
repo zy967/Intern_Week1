29c2ebcdb3ec560b0e1e8cdafd70402bc553a66d
zy967 <ys790094203@gmail.com>
2021-01-10 19:48:12 +0800

    com

--- src/test/java/NodeTest.java
+++ src/test/java/NodeTest.java
@@ -5,40 +5,32 @@ import static org.junit.Assert.*;
 
 public class NodeTest {
 
-    @Before
-    public void Init() {
-        Node<Integer> node = new Node<Integer>(0);
-    }
-
     @Test
-    public void setNext() {
+    public void getAndSetNext() {
         Node<Integer> node = new Node<Integer>(0);
         node.setNext(new Node<Integer>(2));
-        assertEquals(2, node.getNext().getValue().intValue());
+        assertEquals(Integer.valueOf(2), node.getNext().getValue());
     }
 
     @Test
-    public void setPrev() {
+    public void getAndSetPrev() {
         Node<Integer> node = new Node<Integer>(0);
         node.setPrev(new Node<Integer>(2));
-        assertEquals(2, node.getPrev().getValue().intValue());
+        assertEquals(Integer.valueOf(2), node.getPrev().getValue());
     }
-
-    @Test
-    public void getNext() {
-
-    }
-
-    @Test
-    public void getPrev() {
-    }
-
 
     @Test
     public void getValue() {
+        Node<Integer> node = new Node<Integer>(0);
+        assertEquals(Integer.valueOf(0), node.getValue());
     }
 
     @Test
     public void setValue() {
+        Node<Integer> node = new Node<Integer>(0);
+        assertEquals(Integer.valueOf(0), node.getValue());
+        
+        node.setValue(3);
+        assertEquals(Integer.valueOf(3), node.getValue());
     }
 }
